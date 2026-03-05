a0262100dfe7d4257ac8f3e28476296096d65b04
Kyung Min Song <kyungminsong@gmail.com>
2026-03-05 00:08:14 -0800

    add nginx proxy, add dockerignore, add github cicd

--- infra/aws-minimal/notifications.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "alerts" {
-  name = "${local.name_prefix}-alerts"
-}
-
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.alerts.arn
-  protocol  = "email"
-  endpoint  = var.budget_alert_email
-}
